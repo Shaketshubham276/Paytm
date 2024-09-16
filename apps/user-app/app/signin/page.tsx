59917863596644796bd89043321821593d5a3164
Shaketshubham276 <2021ugme023@nitjsr.ac.in>
2024-09-16 12:06:24 +0530

    Second commit

---
+++ apps/user-app/app/signin/page.tsx
@@ -0,0 +1,7 @@
+import { useBalance } from "@repo/store/useBalance";
+export default function(){
+    const balance=useBalance()
+        return <div>
+            hi there {balance}
+        </div>
+    }
